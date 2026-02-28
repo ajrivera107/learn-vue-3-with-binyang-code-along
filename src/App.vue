0df292720f9071ee747a91c08fa59823c01ffa8e
ajrivera107 <ashley.jh.rivera@gmail.com>
2026-02-28 14:35:15 -0600

    Reactivity system

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,84 @@
 <template>
-  <div>
-Hello Vue!
+  <div class="card">
+    <h2>Message: {{ message }}</h2>
+    <h2>Number: {{ number }}</h2>
+    <button @click="changeMessageToUpperCase">
+      Change message to upper case
+    </button>
+    <button @click="incrementNumber">
+      Increment Number
+    </button>
   </div>
-</template>
+  <div class="card">
+    <h2>{{ wizard.name }}</h2>
+    <h2>{{ wizard.wand }}</h2>
+    <button @click="changeNameToUpperCase">Change name to upper case</button>
+    <button @click="changeWandCore">Change wand to upper case</button>
+    <button @click="changeWizard">Change wizard</button>
+  </div>
 
-<script setup>
+  <div class="card">
+    <h2>Array {{ wizards }}</h2>
+    <button @click="wizards.push('Draco')">Add a new wizard</button>
+  </div>
+  </template>
 
+  <script setup>
+  import {ref} from 'vue'
+  let message = ref( 'Hello Reactivity!')
+  let number = ref(1)
+
+  function changeMessageToUpperCase() {
+    message.value = message.value.toUpperCase()
+    console.log(message)
+  }
+
+  function incrementNumber() {
+    number.value +- 1;
+    console.log(number)
+  }
+
+  let wizard = ref({
+    id: 1001,
+    name: 'Harry Potter',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Phoenix feather',
+      wood: 'Holy'
+    }
+  })
+
+  function changeNameToUpperCase() {
+    wizard.value.name = wizard.value.name.toUpperCase()
+  }
+
+  function changeWandCore() {
+    wizard.value.wand.core = 'Unicorn hair'
+  }
+
+  function changeWizard() {
+    wizard.value = {
+      id: 1002,
+      name: 'Hermione Granger',
+      house: 'Gryffindor',
+      age: 17,
+      wand: {
+        core: 'Dragon heartstring',
+        wood: 'Vine'
+    }
+    }
+  }
+
+  let wizards = ref(['Harry', 'Hermoine', 'Ron'])
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.card {
+  background-color: purple;
+  color:white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
 
 </style>
