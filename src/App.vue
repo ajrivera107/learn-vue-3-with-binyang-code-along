a6bd95b38456b439cdcd13c8cb664c02e38680b7
ajrivera107 <ashley.jh.rivera@gmail.com>
2026-03-01 16:28:34 -0600

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>
-Hello Vue!
+  <h1>{{ message }}</h1>
+  <div class="card">
+    <h1 ref="title">This is the App component.</h1>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">
+      Increment number by one
+    </button>
+    <button @click="isShow = !isShow">Toggle Component1</button>
+    <Component1 v-if="isShow"></Component1>
   </div>
 </template>
 
 <script setup>
+import { watch, onBeforeMount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, onBeforeUnmount, ref } from 'vue'
+
+import Component1 from './Component1.vue'
+
+let message = ref('Hello Lifecycle Hooks!')
+
+let number = ref(1)
+let title = ref(null)
+
+let isShow = ref(true)
+
+console.log('App component is setup')
+
+onBeforeMount(() => {
+  console.log('App component is before mount.')
+  console.log(number.value)
+  console.log(title.value)
+}) 
+
+onMounted(() => {
+  console.log('App component is mounted')
+  console.log(title.value)
+})
+
+onBeforeUpdate(() =>{
+  console.log('App component is before update.')
+})
+
+onUpdated(() => {
+  console.log('App component is updated')
+})
+
+onBeforeUnmount(() => {
+  console.log('App component is before unmount')
+})
+
+onUnmounted(() => {
+  console.log('App component is unmounted.')
+})
+
+watch(number, () => {
+  console.log('Number changes')
+})
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.card{
+  background-color: purple;
+  color:white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
 </style>
---
+++ src/Component1.vue
@@ -0,0 +1,49 @@
+<template>
+  <div style="background-color: aqua; padding:10px; margin: 20px">
+    <h2>This is the Component1 component</h2>
+  </div>
+</template>
+
+<script setup>
+import { watch, onBeforeMount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, onBeforeUnmount } from 'vue'
+
+console.log('Component1 is setup.')
+
+onBeforeMount(() => {
+  console.log('Component1 is before mount')
+}) 
+
+onMounted(() => {
+  console.log('Component1 is mounted.')
+})
+
+onBeforeUpdate(() =>{
+  console.log('Component is before update.')
+})
+
+onUpdated(() => {
+  console.log('Component1 is updated.')
+})
+
+onBeforeUnmount(() => {
+  console.log('Component1 is before unmount.')
+})
+
+onUnmounted(() => {
+  console.log('Component1 is unmounted.')
+})
+
+watch(number, () => {
+  console.log('Number changes')
+})
+
+</script>
+
+<style scoped>
+.card{
+  background-color: purple;
+  color:white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
+</style>
